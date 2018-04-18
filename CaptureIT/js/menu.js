8986417340e5fd648da59f6ca5a70daa9b499b70
LazyBoot <github@lazyboot.net>
2018-04-18 13:07:32 +0200

    Meny

--- CaptureIT/js/menu.js
+++ CaptureIT/js/menu.js
@@ -1,2 +1,22 @@
 // JavaScript source code
 'use strict';
+
+var menuDiv;
+var menuWidth;
+var menuPos;
+var extraPadding = 50;
+
+document.addEventListener('DOMContentLoaded', menuOnLoad);
+function menuOnLoad() {
+    menuDiv = document.getElementById('menu');
+    menuWidth = parseInt(window.getComputedStyle(menuDiv).getPropertyValue('width'));
+    menuPos = Math.abs(parseInt(window.getComputedStyle(menuDiv).getPropertyValue('left')) + menuWidth + extraPadding);
+}
+
+function menuOpen() {
+    menuDiv.style.left = menuPos + 'px';
+}
+
+function menuClose() {
+    menuDiv.style.left = '-' + (menuWidth + extraPadding) + 'px';
+}
