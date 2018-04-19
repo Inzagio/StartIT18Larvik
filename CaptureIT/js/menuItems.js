981b6f4e8abe7d6d56a644ce6cbe089e4e5ff269
LazyBoot <github@lazyboot.net>
2018-04-19 15:45:46 +0200

    Changed menu to a module where only some functions are public. Part 2

--- CaptureIT/js/menuItems.js
+++ CaptureIT/js/menuItems.js
@@ -3,12 +3,12 @@
 
 function getMenuItems() {
     if (isLoggedIn)
-        return '<a href="javascript:void(0)" class="closebtn" onclick="menuClose()">&times;</a> \
-            <a href="javascript:void(0)" onclick="logout()"><b>Logout</b></a> \
+        return '<a href="javascript:void(0)" class="closebtn" onclick="menu.menuClose()">&times;</a> \
+            <a href="javascript:void(0)" onclick="menu.logout()"><b>Logout</b></a> \
             <a href="javascript:void(0)" onclick="">Savings/Loans</a> \
             <a href="javascript:void(0)" onclick="setupStatus();">Status</a> \
             <a href="javascript:void(0)" onclick="admin_panel()">Admin</a>';
     else
-        return ' <a href="javascript:void(0)" class="closebtn" onclick="menuClose()">&times;</a> \
-            <a href="javascript:void(0)" onclick="loginShow();"><b>Login</b></a>';
+        return ' <a href="javascript:void(0)" class="closebtn" onclick="menu.menuClose()">&times;</a> \
+            <a href="javascript:void(0)" onclick="menu.loginShow();"><b>Login</b></a>';
 }
