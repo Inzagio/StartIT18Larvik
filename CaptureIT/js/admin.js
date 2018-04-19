48df31a73e30585a6328acda3d9634f718da0422
Trym <chilled-1337@hotmail.com>
2018-04-19 10:33:01 +0200

    Updates to index to fix admin js
    Added admin css.

--- CaptureIT/js/admin.js
+++ CaptureIT/js/admin.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 function admin_panel() {
     wrapper();
-  
+    //header();
     side_bar();
     main_content();
     footer();
@@ -17,32 +17,38 @@ function admin_panel() {
 function wrapper() {
     var wrapper = document.createElement("div");
     wrapper.setAttribute("id", "wrapper");
+    wrapper.classList.add("wrapper");
     replacedNode = showcase.replaceChild(wrapper, container);
 }
 
 function side_bar() {
     var sidebar = document.createElement("div");
     sidebar.innerHTML = "Admin sidebar";
-    sidebar.setAttribute("class", "sidebar");
+    sidebar.setAttribute("id", "sidebar");
+    sidebar.classList.add("sidebar");
     document.getElementById("wrapper").appendChild(sidebar);
 }
 //function header() {
 //    var header = document.createElement("header");
 //    header.innerHTML = "Header";
-//    header.setAttribute("class", "header");
+//    header.setAttribute("id", "header");
+//    header.classList.add("header");
+
 //    document.getElementById("wrapper").appendChild(header);
 //}
 function main_content () {
     var mainContent = document.createElement("div");
     mainContent.innerHTML = "lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum lorem impsum"
-    mainContent.setAttribute("class", "content");
+    mainContent.classList.add("content");
+    mainContent.setAttribute("id", "content");
     document.getElementById("wrapper").appendChild(mainContent);
 }
 
 function footer() {
     var footer = document.createElement("footer");
     footer.innerHTML = "Copyright Start IT 2018 Larvik & ReNewGen AS";
-    footer.setAttribute("class", "footer");
+    footer.setAttribute("id", "footer");
+    footer.classList.add("footer");
     document.getElementById("wrapper").appendChild(footer);
     
 }
