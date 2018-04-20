98c39646c6a61786385106f05a0d57753efbc455
Unknown <aasmholm@gmail.com>
2018-04-20 13:40:38 +0200

    Auto stash before merge of "master" and "origin/master"

--- CaptureIT/js/globalVars.js
+++ CaptureIT/js/globalVars.js
@@ -1,3 +1,13 @@
 // JavaScript source code
 
-var isLoggedIn = true; // TODO: Set false før release.
+var isLoggedIn = true; // TODO: Set false fï¿½r release.
+
+// Global values for use in calculations and displaying data
+
+const currency = 'TZS';
+//Defines what one share costs in the selected currency 
+const sharesValue = 10;
+//Defines the community payment (minimum ammount to save each month)
+const communityValue = 20;
+//Defines what a user is allowed to borrow (users savings * toLend)
+const toLend = 3;
