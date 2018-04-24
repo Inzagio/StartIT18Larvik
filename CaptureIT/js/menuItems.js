6d27c017c7da9f284a4f02a3bbbf5ead75513780
leidsten <38457627+leidsten@users.noreply.github.com>
2018-04-24 11:16:40 +0200

    Added register to menu

--- CaptureIT/js/menuItems.js
+++ CaptureIT/js/menuItems.js
@@ -6,6 +6,7 @@ function getMenuItems() {
         return ['<a href="javascript:void(0)" onclick="menu.logout()"><b>Logout</b></a>',
             '<a href="javascript:void(0)" onclick="">Savings/Loans</a>',
             '<a href="javascript:void(0)" onclick="setupStatus();">Status</a>',
+			 '<a href="javascript:void(0)" onclick="registerTransaction();">Register Transaction</a>',
             '<a href="javascript:void(0)" onclick="admin_panel()">Admin</a>'];
     else
         return ['<a href="javascript:void(0)" onclick="menu.loginShow();"><b>Login</b></a>'];
