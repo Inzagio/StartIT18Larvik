32348d8fcaa06190393b8408e10fa6f89a3d06f7
LazyBoot <github@lazyboot.net>
2018-04-27 12:53:46 +0200

    Changed order of menu items based on feedback.

--- CaptureIT/js/menuItems.js
+++ CaptureIT/js/menuItems.js
@@ -2,10 +2,10 @@
 'use strict';
 
 var menuItems = [
-    '<a href="javascript:void(0)" onclick="menu.logout()"><b>Logout</b></a>',
-    '<a href="javascript:void(0)" onclick="setupStatus();">Status</a>',
     '<a href="javascript:void(0)" onclick="registerTransaction();">Register Transaction</a>',
-    '<a href="javascript:void(0)" onclick="admin_panel()">Admin</a>'
+    '<a href="javascript:void(0)" onclick="setupStatus();">Status</a>',
+    '<a href="javascript:void(0)" onclick="admin_panel()">Admin</a>',
+    '<a href="javascript:void(0)" onclick="menu.logout()"><b>Logout</b></a>'
 ];
 
 const defaultItems = menuItems.slice();
