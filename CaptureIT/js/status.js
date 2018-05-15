e5eb4e4945954677d3b0a873cb42996509ee4947
LazyBoot <github@lazyboot.net>
2018-05-15 11:41:00 +0200

    First draft of status page.

--- CaptureIT/js/status.js
+++ CaptureIT/js/status.js
@@ -12,9 +12,58 @@ function setupStatus() {
 
     if (isLoggedIn) {
         menu.setBreadcrumbs('Status');
-        container.innerHTML = '<h1>Logged In</h1>';
+        var currentUser = {
+            paidSocial: 10002,
+            boughtShares: 200,
+            loanSocial: 5000,
+            loanShares: 200
+        }
+
+        var statusTable = createStatusTable(container);
+
+        statusTable.paidSocialField.innerHTML = currentUser.paidSocial + currency;
+        statusTable.boughtSharesField.innerHTML = currentUser.boughtShares + currency;
+        statusTable.loanSocialField.innerHTML = currentUser.loanSocial + currency;
+        statusTable.loanSharesField.innerHTML = currentUser.loanShares + currency;
     }
     else {
         setupStartPage();
     }
+}
+
+function createStatusTable(container) {
+    var wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    var table = document.createElement('table');
+    container.appendChild(wrapper);
+    wrapper.appendChild(table);
+    var row;
+    var cell;
+
+    row = table.insertRow();
+    cell = row.insertCell();
+    cell.innerHTML = 'Paid into Social Funds';
+    var paidSocialField = row.insertCell();
+
+    row = table.insertRow();
+    cell = row.insertCell();
+    cell.innerHTML = 'Bought shares';
+    var boughtSharesField = row.insertCell();
+
+    row = table.insertRow();
+    cell = row.insertCell();
+    cell.innerHTML = 'Loan from Social Funds';
+    var loanSocialField = row.insertCell();
+
+    row = table.insertRow();
+    cell = row.insertCell();
+    cell.innerHTML = 'Loan from Shares';
+    var loanSharesField = row.insertCell();
+
+    return {
+        paidSocialField,
+        boughtSharesField,
+        loanSocialField,
+        loanSharesField
+    };
 }
